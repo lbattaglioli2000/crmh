194ffb69753ad737f589db72563ff46546f1581e
Luigi Battaglioli <him@theluigi.com>
2023-06-27 21:36:08 -0400

    cleaned up mobile styles a bit

--- resources/js/site.js
+++ resources/js/site.js
@@ -69,3 +69,14 @@ $(function () {
         });
     });
 });
+
+$(function () {
+    // smooth scroll to locations
+    $('a[href*="#"]').on('click', function (e) {
+        e.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top - 50
+        }, 500, 'linear');
+    });
+});
